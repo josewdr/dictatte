b54509e3d2848c47a1575eed51333778abe85666
josewdr <50268140+josewdr@users.noreply.github.com>
2022-06-05 06:42:29 -0600

    se incluve variable global PORT

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,7 @@
+//Buscan un arvhico .dev y si existe utiliza el puerto indicado ahí, si no existe utiliza el puerto 3000
+
+require('dotenv').config()
+
 //Crear un servidor con Express
 const express = require('express');
 const app = express();
@@ -6,9 +10,11 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 }); //end app.get
 
-//Abre el servidor en el puerto 3000
-app.listen(3000, () => {
-    console.log('Server on port 3000');
-}); //end app.listen
+//Referenciar el puerto a una vaiable de entorno PORT
+const PORT = process.env.PORT;
 
-////////////////////////////////////////////////////////////////////////////////////////////
+//Iniciar el servidor
+//Abre el servidor en el puerto 3000
+app.listen(PORT, () => {
+    console.log('Server on port' + PORT);
+}); //end app.listen
