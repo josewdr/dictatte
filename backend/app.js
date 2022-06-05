ba617c24627cdc00053ab0ad031c48474ad24ade
josewdr <50268140+josewdr@users.noreply.github.com>
2022-06-04 21:51:06 -0600

    primer commit

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+//Crear un servidor con Express
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}); //end app.get
+
+//Abre el servidor en el puerto 3000
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+}); //end app.listen
+
+////
